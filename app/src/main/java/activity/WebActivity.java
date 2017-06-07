7e1e457e9f9b6029698873a4177fbde3243bb6e7
helen <852568775@qq.com>
2017-06-07 11:24:28 +0800

    2017 6 6 Helen11

--- app/src/main/java/activity/WebActivity.java
+++ app/src/main/java/activity/WebActivity.java
@@ -1,19 +1,28 @@
 package activity;
 
+import android.webkit.WebView;
+
+import com.example.snoy.helen.R;
+
 import base.HBaseActivity;
 
 /**
  * Created by Helen on 2017/6/7.
  */
 public class WebActivity extends HBaseActivity {
+
+
+    private WebView webView;
+
+
     @Override
     public int getContentView() {
-        return 0;
+        return R.layout.activity_web;
     }
 
     @Override
     public void findViews() {
-
+        webView = (WebView)contentView. findViewById(R.id.web_view);
     }
 
     @Override
